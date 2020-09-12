7bb40c8fd818f915ad113b409c6d7da74d946b8e
mGammal <mohamed.gammal@mideastsoft.com>
2020-09-12 14:10:07 +0200

    GADS-20200912 Implement Error valdation message

--- app/src/main/java/com/example/gadsleaderboardmobileapplication/ui/submit/SubmitActivity.java
+++ app/src/main/java/com/example/gadsleaderboardmobileapplication/ui/submit/SubmitActivity.java
@@ -2,18 +2,21 @@ package com.example.gadsleaderboardmobileapplication.ui.submit;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.gadsleaderboardmobileapplication.R;
 import com.example.gadsleaderboardmobileapplication.databinding.ActivitySubmitBinding;
 import com.example.gadsleaderboardmobileapplication.utils.ConnectivityManager;
+import com.github.florent37.viewtooltip.ViewTooltip;
 
 import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static com.example.gadsleaderboardmobileapplication.utils.Validation.isEmailValid;
 
@@ -23,6 +26,7 @@ public class SubmitActivity extends AppCompatActivity {
     private AlertDialog alertDialog;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         vInit();
@@ -35,11 +39,18 @@ public class SubmitActivity extends AppCompatActivity {
     }
 
     private void vHandleSubmissionResponse(Integer integer) {
+        activitySubmitBinding.progressBar.setVisibility(View.GONE);
         if (integer == 1) {
             ConnectivityManager.vShowSuccessFailDialogue(this, true);
         } else {
             ConnectivityManager.vShowSuccessFailDialogue(this, false);
         }
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                onBackPressed();
+            }
+        }, 3000);
     }
 
     private void vHandleOnClickActions() {
@@ -51,20 +62,34 @@ public class SubmitActivity extends AppCompatActivity {
     }
 
     private void vHandleSubmitClick() {
-        if (!Objects.requireNonNull(activitySubmitBinding.editTxtFName.getText()).toString().equals("") &&
-                !Objects.requireNonNull(activitySubmitBinding.editTxtLName.getText()).toString().equals("") &&
-                !Objects.requireNonNull(activitySubmitBinding.editTxtEmail.getText()).toString().equals("") &&
-                !Objects.requireNonNull(activitySubmitBinding.editTxtProjectLink.getText()).toString().equals("") &&
-                isEmailValid(Objects.requireNonNull(activitySubmitBinding.editTxtEmail.getText()).toString())) {
+        if (Objects.requireNonNull(activitySubmitBinding.editTxtFName.getText()).toString().equals("")) {
+            setError(activitySubmitBinding.editTxtFName, "Please enter your first name");
+        } else if (Objects.requireNonNull(activitySubmitBinding.editTxtLName.getText()).toString().equals("")) {
+            setError(activitySubmitBinding.editTxtLName, "Please enter your last name");
+        } else if (Objects.requireNonNull(activitySubmitBinding.editTxtEmail.getText()).toString().equals("")) {
+            setError(activitySubmitBinding.editTxtEmail, "Please enter your email");
+        } else if (!isEmailValid(Objects.requireNonNull(activitySubmitBinding.editTxtEmail.getText()).toString())) {
+            setError(activitySubmitBinding.editTxtEmail, "Please enter valid email");
+        } else if (Objects.requireNonNull(activitySubmitBinding.editTxtProjectLink.getText()).toString().equals("")) {
+            setError(activitySubmitBinding.editTxtProjectLink, "Please enter your project link");
+        } else {
             alertDialog = ConnectivityManager.vShowConfirmationDialogue(this, view -> {
                 alertDialog.dismiss();
                 vCallSubmitAPI();
             });
-
-        } else {
-            Toast.makeText(this, "Please complete cradentials ",
-                    Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void setError(AppCompatEditText editText, String error) {
+        ViewTooltip
+                .on(editText)
+                .corner(40)
+                .color(getResources().getColor(R.color.errorTextColor, null))
+                .position(ViewTooltip.Position.TOP)
+                .autoHide(true, 2000)
+                .text(error)
+                .clickToHide(true)
+                .show();
     }
 
     private void vCallSubmitAPI() {
